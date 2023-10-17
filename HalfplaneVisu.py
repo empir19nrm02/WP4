8258d82edb82983b36054598b899a1b4fb500242
Leonhard Dudzik <Dudzik@lti.local>
2023-10-17 17:11:48 +0200

    interactive Visu for Monte Carlo, new interface

--- HalfplaneVisu.py
+++ HalfplaneVisu.py
@@ -1,140 +1,210 @@
+import itertools
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid1.inset_locator import InsetPosition
 from mpl_toolkits.axes_grid1.parasite_axes import HostAxes
 from IPython import get_ipython
-#import mpl_toolkits.axisartist as AA
+from IPython.display import display
 
-class HalfplaneAxes:
-    def __init__(self,fig = None, cart = True, polar = False, cart_aux = False, label_aux = None,
-                 theta_max=90, theta_tick_dist=15,r_max = 115, r_min=-15):
-        # disable tight bbox from Ipython magic
-        get_ipython().run_line_magic('config',"InlineBackend.print_figure_kwargs = {'bbox_inches':None}")
-        
-        # make sure there is a figure
-        if(fig == None):
-            fig = plt.figure(figsize=(12,8))
-        self.fig = fig
-        
-        # make sure there is at least one axes. if neither polar nor cart is specified, default to cartesian plot
-        if(polar == False):
-            cart = True
-        if(cart == False):
-            polar = True
-            
-        self.cart = cart
-        self.polar = polar
-        self.cart_aux = cart_aux
-        
+class HalfplaneFigure:
+    def __init__(self, cart, polar, cart_aux, theta_max, theta_tick_dist, r_max, r_min):
+        self.fig = plt.figure(figsize=(12,8))
         # create the axes and store them in member variables
         (self.ax_cart,
          self.ax_polar,
-         self.ax_cart_aux) = create_axes_cplane(fig, cart, polar, cart_aux, label_aux, theta_max, theta_tick_dist, r_max, r_min)
-        plt.close(fig)
+         self.ax_cart_aux) = create_axes_cplane(self.fig, cart, polar, cart_aux, theta_max, theta_tick_dist, r_max, r_min)
+        plt.close(self.fig)
+        
         # create a dictionary of lines plotted to the different axes: one for combined cart/polar and one for the auxilary axis
         # key is the label, value is a line-object for the aus plots and a dictionary of line objects (polar,cart) for the main plots
         self.plots = {}
         self.plots_aux = {}
         
-        # set a variable for the auxilary axis interface
-        self.aux_center = 0
-        
-    def get_axes_handles(self):
-        return self.ax_cart, self.ax_polar, self.ax_cart_aux
-    
-    def plot(self, theta, r, label, **kwargs):
-        theta = np.array(theta).squeeze()
-        if(self.cart == True):
-            if(self.polar == True):
-                # split up into left polar and right cartesian plot
-                # left side:
-                pos_theta_indices = np.where(theta >= 0)
-                x_cart = np.rad2deg(theta[pos_theta_indices])
-                y_cart= r[pos_theta_indices]
-                
-                # right side:
-                neg_theta_indices = np.where(theta <= 0)
-                theta_polar = theta[neg_theta_indices]
-                r_polar = r[neg_theta_indices]
-                
-                # test, if the label was already plotted:
-                if label in self.plots:
-                    # line data needs to be updated
-                    self.plots[label]['cart'].set_data(x_cart,y_cart)
-                    self.plots[label]['polar'].set_data(theta_polar, r_polar)
-                else:
-                    # new plots
-                    line_cart = self.ax_cart.plot(x_cart, y_cart, label=label, **kwargs)
-                    line_polar = self.ax_polar.plot(theta_polar, r_polar, label=label, **kwargs)
-                    # add this plot to the dictionary
-                    self.plots[label] = {'cart':line_cart[0], 'polar':line_polar[0]}
-            else:
-                # only plot cart
-                x_cart = np.rad2deg(theta)
-                y_cart = r
-                if label in self.plots:
-                    self.plots[label]['cart'].set_data(x_cart,y_cart)
-                else:
-                    line_cart = self.ax_cart.plot(x_cart, y_cart, label=label, **kwargs)
-                    self.plots[label] = {'cart':line_cart[0], 'polar':None}
-        else:
-            # only polar
-            if label in self.plots:
-                self.plots[label]['polar'].set_data(theta,r)
-            else:
-                line_polar = self.ax_polar.plot(theta, r, label=label, **kwargs)
-                self.plots[label] = {'cart':None, 'polar':line_polar[0]}
-    
-    def plot_aux(self, theta, aux, label, **kwargs):
-        theta = np.array(theta).squeeze()
-        if(self.polar == True and self.cart == True):
-            # only plot to the right side
-            pos_theta_indices = np.where(theta >= 0)
-            x_cart = np.rad2deg(theta[pos_theta_indices])
-            y_cart= aux[pos_theta_indices]
-        else:
-            x_cart = np.rad2deg(theta)
-            y_cart = aux
-        
-        if label in self.plots_aux:
-            self.plots_aux[label].set_data(x_cart,y_cart)
-        else:
-            line_cart = self.ax_cart_aux.plot(x_cart, y_cart, label=label, **kwargs)
-            self.plots_aux[label] = line_cart[0]
-            
-        self.__rescale_aux()
-    
-
-    def aux_set_center(self,center):
-        self.aux_center = center
-        self.__rescale_aux()
-    
-    def aux_set_label(self,label):
-        self.ax_cart_aux.set_ylabel(axis_label, size=14, labelpad=-22, y=1.06, rotation=0) # -38 for rel i0
-    
-    def __rescale_aux(self):
+    def rescale_aux(self,center):
         if self.ax_cart_aux == None:
             return
         self.ax_cart_aux.relim() # updates the dataLim member for the current data
         damp_y2 = 0.8 # damp so that the plots to ax_cart_aux don't get too tight
-        ylim = np.array(self.ax_cart.get_ylim())
-        ratio_ylim = ylim[0] / ylim[1] # min / max
+        ylim_min, ylim_max = np.array(self.ax_cart.get_ylim())
+        ratio_ylim = ylim_min / ylim_max
         
         #ylim2 = np.array(self.ax_cart_aux.get_ylim())
         ylim2 = np.array(self.ax_cart_aux.dataLim).T[1]
-        ylim2 -= self.aux_center # substract value to center, now min max are centered around 0
+        ylim2 -= center # substract value to center, now min max are centered around 0
         ylim2_min = ylim2[0]
         ylim2_max = ylim2[1]
-        if(np.abs(ylim2_max) > np.abs(ylim2_min)): # if max lim is bigger than min lim
+        if(np.abs(ylim2_max / ylim_max)  > np.abs(ylim2_min / ylim_min)): # which side is more filled out?
             ylim2_max = ylim2_max / damp_y2
             ylim2_min = ylim2_max * ratio_ylim
         else:
             ylim2_min = ylim2_min / damp_y2
             ylim2_max = ylim2_min / ratio_ylim
-        self.ax_cart_aux.set_ylim(bottom = ylim2_min + self.aux_center, top = ylim2_max + self.aux_center)
+        self.ax_cart_aux.set_ylim(bottom = ylim2_min + center, top = ylim2_max + center)
+        
+    def update_legend(self,loc):
+        legend_items = []
+        # get fills (collections)
+        for collection in itertools.chain(self.ax_cart.collections, self.ax_polar.collections):
+            legend_items.append(collection)
+        for plot in self.plots.values():
+            line = plot['cart'] if isinstance(plot, dict) else plot
+            legend_items.append(line)
+        self.ax_cart.legend(legend_items, [item.get_label() for item in legend_items],
+                            loc=loc)
 
+class HalfplaneVisu:
+    def __init__(self, theta_max=90, theta_tick_dist=15,r_max = 115, r_min=-15):
+        # disable tight bbox from Ipython magic
+        get_ipython().run_line_magic('config',"InlineBackend.print_figure_kwargs = {'bbox_inches':None}")
+        
+        # create three figures: with cart, polar and cartpolar axes
+        self.fig_cart      = HalfplaneFigure(True , False, True , theta_max, theta_tick_dist, r_max, r_min)
+        self.fig_polar     = HalfplaneFigure(False, True , False, theta_max, theta_tick_dist, r_max, r_min)
+        self.fig_cartpolar = HalfplaneFigure(True , True , True , theta_max, theta_tick_dist, r_max, r_min)
+        
+        # prepare legend
+        self.legend_loc = (0.0,0.0) # location of the legend in axis-coordinates
 
-def create_axes_cplane(fig, cart, polar, cart_aux, label_aux, theta_max, theta_tick_dist,r_max, r_min):
+        # set a variable for the auxilary axis interface
+        self.aux_center = 0
+    
+    def show_fig(self, cart, polar):
+        if(polar == True):
+            if(cart == True):
+                display(self.fig_cartpolar.fig)
+            else:
+                display(self.fig_polar.fig)
+        else:
+            display(self.fig_cart.fig)
+    
+    def plot(self, theta, r, label, **kwargs):
+        theta = np.array(theta).squeeze()
+        # plot to cartesian figure
+        x_cart = np.rad2deg(theta)
+        y_cart = r
+        if label in self.fig_cart.plots:
+            self.fig_cart.plots[label].set_data(x_cart,y_cart)
+        else:
+            line_cart = self.fig_cart.ax_cart.plot(x_cart, y_cart, label=label, **kwargs)
+            self.fig_cart.plots[label] = line_cart[0]
+            self.fig_cart.update_legend(self.legend_loc)
+
+        # plot to cart/polar figure
+        # split up into left polar and right cartesian plot
+        # left side:
+        pos_theta_indices = np.where(theta >= 0)
+        x_cart = np.rad2deg(theta[pos_theta_indices])
+        y_cart= r[pos_theta_indices]
+        
+        # right side:
+        neg_theta_indices = np.where(theta <= 0)
+        theta_polar = theta[neg_theta_indices]
+        r_polar = r[neg_theta_indices]
+        if label in self.fig_cartpolar.plots:
+            # line data needs to be updated
+            self.fig_cartpolar.plots[label]['cart'].set_data(x_cart,y_cart)
+            self.fig_cartpolar.plots[label]['polar'].set_data(theta_polar, r_polar)
+        else:
+            # new plots
+            line_cart  = self.fig_cartpolar.ax_cart.plot(x_cart, y_cart, label=label, **kwargs)
+            line_polar = self.fig_cartpolar.ax_polar.plot(theta_polar, r_polar, label=label, **kwargs)
+            # add this plot to the dictionary
+            self.fig_cartpolar.plots[label] = {'cart':line_cart[0], 'polar':line_polar[0]}
+            self.fig_cartpolar.update_legend(self.legend_loc)
+        
+        # plot to polar figure
+        if label in self.fig_polar.plots:
+            self.fig_polar.plots[label].set_data(theta,r)
+        else:
+            line_polar = self.fig_polar.ax_polar.plot(theta, r, label=label, **kwargs)
+            self.fig_polar.plots[label] = line_polar[0]
+            self.fig_polar.update_legend(self.legend_loc)
+    
+    def fill(self,theta,r1,r2,label,**kwargs):
+        # if no alpha was specified, implement default transparency
+        if 'alpha' not in kwargs:
+            kwargs['alpha'] = 0.2
+        theta = np.array(theta).squeeze()
+        # fill creates a polygon collection. there is no set_data equivalent so we have to delete and redraw
+        # check if there already are fills with the given label, if yes delete them
+        figs = [self.fig_cart, self.fig_polar, self.fig_cartpolar]
+        for fig in figs:
+            for collection in itertools.chain(fig.ax_cart.collections, fig.ax_polar.collections):
+                if str(collection.get_label()) == label:
+                    collection.remove()
+                del collection
+        # then plot the fill according to the given axes
+        # plot to cartesian figure
+        x_cart = np.rad2deg(theta)
+        y_cart1 = r1
+        y_cart2 = r2
+        fill_object = self.fig_cart.ax_cart.fill_between(x_cart, y_cart1, y_cart2, label=label, **kwargs)
+        self.fig_cart.update_legend(self.legend_loc)
+        
+        # plot to cart/polar figure
+        # split up into left polar and right cartesian plot
+        # left side:
+        pos_theta_indices = np.where(theta >= 0)
+        x_cart = np.rad2deg(theta[pos_theta_indices])
+        y_cart1= r1[pos_theta_indices]
+        y_cart2= r2[pos_theta_indices]
+        # right side:
+        neg_theta_indices = np.where(theta <= 0)
+        theta_polar = theta[neg_theta_indices]
+        r_polar1 = r1[neg_theta_indices]
+        r_polar2 = r2[neg_theta_indices]
+        fill_object = self.fig_cartpolar.ax_cart.fill_between(x_cart, y_cart1, y_cart2, label=label, **kwargs)
+        self.fig_cartpolar.ax_polar.fill_between(theta_polar, r_polar1, r_polar2, label=label, **kwargs)
+        self.fig_cartpolar.update_legend(self.legend_loc)
+        
+        # plot to polar figure
+        fill_object = self.fig_polar.ax_polar.fill_between(theta, r1,r2, label=label, **kwargs)
+        self.fig_polar.update_legend(self.legend_loc)
+    
+    def plot_aux(self, theta, aux, label, **kwargs):
+        theta = np.array(theta).squeeze()
+        
+        # plot to cartesian figure
+        x_cart = np.rad2deg(theta)
+        y_cart = aux
+        if label in self.fig_cart.plots_aux:
+            self.fig_cart.plots_aux[label].set_data(x_cart,y_cart)
+        else:
+            line_cart = self.fig_cart.ax_cart_aux.plot(x_cart, y_cart, label=label, **kwargs)
+            self.fig_cart.plots_aux[label] = line_cart[0]
+            self.fig_cart.update_legend(self.legend_loc)
+        
+        # plot to cart / polar figure
+        # only plot to the right side
+        pos_theta_indices = np.where(theta >= 0)
+        x_cart = np.rad2deg(theta[pos_theta_indices])
+        y_cart= aux[pos_theta_indices]
+        
+        if label in self.fig_cartpolar.plots_aux:
+            self.fig_cartpolar.plots_aux[label].set_data(x_cart,y_cart)
+        else:
+            line_cart = self.fig_cartpolar.ax_cart_aux.plot(x_cart, y_cart, label=label, **kwargs)
+            self.fig_cartpolar.plots_aux[label] = line_cart[0]
+            self.fig_cartpolar.update_legend(self.legend_loc)
+            
+        self.aux_set_center(self.aux_center)
+    
+    def aux_set_center(self,center):
+        self.aux_center = center
+        self.fig_cart.rescale_aux(center)
+        self.fig_cartpolar.rescale_aux(center)
+    
+    def aux_set_label(self,label):
+        self.fig_cart.ax_cart_aux.set_ylabel(axis_label, size=14, labelpad=-22, y=1.06, rotation=0) # -38 for rel i0
+        self.fig_cartpolar.ax_cart_aux.set_ylabel(axis_label, size=14, labelpad=-22, y=1.06, rotation=0) # -38 for rel i0
+
+    def set_legend_loc(self,loc):
+        self.legend_loc = loc
+        self.fig_cart.update_legend(self.legend_loc)
+        self.fig_cartpolar.update_legend(self.legend_loc)
+        self.fig_polar.update_legend(self.legend_loc)
+
+def create_axes_cplane(fig, cart, polar, cart_aux, theta_max, theta_tick_dist,r_max, r_min):
     plot_limits =  [0.05, 0.05, 0.80, 0.9] # realtive to figure size
     
     # create cartesian axes
@@ -252,7 +322,5 @@ def create_axes_cplane(fig, cart, polar, cart_aux, label_aux, theta_max, theta_t
         ax_cart_aux.plot((1.08), (1), marker='^', transform=ax_cart_aux.transAxes, **arrow_fmt)
         ax_cart_aux.tick_params(axis='x', which='minor', bottom=False) # strangely host minor ticks are visible otherwise 
         ax_cart_aux.get_yaxis().get_major_formatter().set_useOffset(False)
-        if (label_aux != None):
-            self.ax_cart_aux.set_ylabel(axis_label, size=14, labelpad=-22, y=1.06, rotation=0) # -38 for rel i0
         
     return ax_cart, ax_polar, ax_cart_aux
